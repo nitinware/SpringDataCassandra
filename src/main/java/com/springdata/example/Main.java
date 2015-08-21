fc68011b2be84bcedbaeb69a27ef832b8e9e6cdb
nitinware <nitinware@gmail.com>
2015-08-20 21:22:48 -0400

    Added an example for insert using CassandraOperations.

--- src/main/java/com/springdata/example/Main.java
+++ src/main/java/com/springdata/example/Main.java
@@ -1,11 +1,13 @@
 package com.springdata.example;
 
+import java.util.Date;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.data.cassandra.core.CassandraOperations;
 
+import com.datastax.driver.core.ConsistencyLevel;
+import com.datastax.driver.core.querybuilder.Insert;
 import com.datastax.driver.core.querybuilder.QueryBuilder;
-import com.datastax.driver.core.querybuilder.Select;
 import com.springdata.example.model.Person;
 
 public class Main {
@@ -13,13 +15,30 @@ public class Main {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:application-context.xml");
 		CassandraOperations cassandraOperations = context.getBean("cassandraTemplate", CassandraOperations.class);
-		insertPerson(cassandraOperations);
-		selectPerson(cassandraOperations);
+		//insertPerson(cassandraOperations);
+		Person person = selectPerson(cassandraOperations);
+		insertLoginEventByQueryBuilder(cassandraOperations, person);
 	}
 
-	private static void selectPerson(CassandraOperations cassandraOperations) {
-		Select select = QueryBuilder.select().from("person");
-		System.out.println("Person ID - " + cassandraOperations.queryForObject(select, Person.class));
+	private static void insertLoginEventByQueryBuilder(
+			CassandraOperations cassandraOperations, Person person) {
+		
+		Insert insert = QueryBuilder.insertInto("login_event");
+			   insert.setConsistencyLevel(ConsistencyLevel.ONE);
+			   insert.value("person_id", person.getId());
+			   insert.value("event_time", new Date());
+			   insert.value("event_code", 1);
+			   insert.value("ip_address", "localhost");
+			   
+		cassandraOperations.execute(insert);
+		
+	}
+
+	private static Person selectPerson(CassandraOperations cassandraOperations) {
+		String cqlOne = "select * from person where id = '1234567890'";
+		Person person = cassandraOperations.selectOne(cqlOne, Person.class);
+		System.out.println(String.format("Found People with Name [%s] for id [%s]", person.getName(), person.getId()));
+		return person;
 	}
 
 	private static void insertPerson(CassandraOperations cassandraOperations) {
